ecb6b8035fbaf20ff6466b25c1fb95e2320697f0
joshsj89 <joshsj89@gmail.com>
2024-02-17 15:18:56 -0800

    Move Power Chair Model to Home

--- src/App.js
+++ src/App.js
@@ -8,11 +8,6 @@ import NavBar from './Components/NavBar/NavBar';
 import { Canvas } from "@react-three/fiber";
 import { useGLTF, Stage, PresentationControlProps, PresentationControls } from '@react-three/drei';
 
-function Model(props){
-  const { scene } = useGLTF("/wheelchairv0.glb");
-  return <primitive object={scene} {...props} />
-}
-
 function App() {
   return (
     <div className="App">
@@ -23,14 +18,6 @@ function App() {
         <Route path="get-involved" element={ <GetInvolved/> } />
         <Route path="merchandise" element={ <Merchandise/> } />
       </Routes>
-      <Canvas dpr={[1,2]} shadows camera ={{ fov: 45 }} style={{"position": "absolute", width: "500px", height: "500px"}}>
-        {/* <color attach="background" args={["#101010"]} /> */}
-        <PresentationControls speed={1.5} global zoom={.5} polar={[-0.1, Math.PI/4]}>
-          <Stage environment={"sunset"}>
-            <Model scale = {0.007}/>
-          </Stage>
-        </PresentationControls>
-      </Canvas>
     </div>
   );
 }
